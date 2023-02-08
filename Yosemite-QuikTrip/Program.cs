f79e5f1c13c61dd76b4a9a4fdfdbb0cda4e9e840
Ethan Sawicki <ethansawicki2797@gmail.com>
2023-02-07 19:18:35 -0600

    Added a do/while to keep program running until exit selected

--- Yosemite-QuikTrip/Program.cs
+++ Yosemite-QuikTrip/Program.cs
@@ -1,13 +1,18 @@
 ﻿void Main()
 {
-    Console.WriteLine("QuikTrip Management Systems");
-    Console.WriteLine();
-    Console.WriteLine(@"
-1. Enter District Sales
-2. Generate District Report
-3. Add New Employee
-4. Add a Store/District
-5. Exit");
-    string userInput = Console.ReadLine();
+    string userInput;
+    do
+    {
+        Console.WriteLine("QuikTrip Management Systems");
+        Console.WriteLine();
+        Console.WriteLine(@"
+    1. Enter District Sales
+    2. Generate District Report
+    3. Add New Employee
+    4. Add a Store/District
+    5. Exit");
+
+        userInput = Console.ReadLine();
+    } while (userInput != "5");
 };
 Main();
