31e7cd2b775630d01f89b5a3d238c78149c838cd
Erica Clayton <erica.baertsch@gmail.com>
2023-02-13 17:59:23 -0600

    added in the refactor for the menu

--- Yosemite-QuikTrip/Employee.cs
+++ Yosemite-QuikTrip/Employee.cs
@@ -26,11 +26,82 @@ public class Employee
     public int Store { get; set; }
     public string District { get; set; }
 
-    public List<Employee> Employees { get; set; }//= new List<Employee>();
-    //public void AddEmployee(Employee employee)
-    //{
-    //    Employees.Add(employee);
-    //}
+    public List<Employee> _employees { get; set; }//= new List<Employee>();
+                                                 //public void AddEmployee(Employee employee)
+                                                 //{
+                                                 //    Employees.Add(employee);
+                                                 //}
+
+    public int NumberValidation(string input)
+    {
+
+        int ValidNumber = 0;
+        //string input = Console.ReadLine();
+        bool isInvalid, isOutOfRange;
+        isOutOfRange = true;
+        do
+        {
+            isInvalid = !int.TryParse(input, out ValidNumber);
+            if (!isInvalid)
+            {
+                isOutOfRange = (ValidNumber < 1 || ValidNumber > 1000);
+            }
+           
+            if (isInvalid)
+            {
+                Console.Write("Please enter a valid Number value: #");
+                input = Console.ReadLine();
+
+            }
+            else if (isOutOfRange)
+            {
+                Console.Write("Please enter value between 1 and 1000: #");
+                input = Console.ReadLine();
+            }
+
+        } while (isInvalid || isOutOfRange);
+
+        return ValidNumber;
+        //newEmployee.Store = ValidNumber;
+    }
+
+    /*---------------//valid Name--------------------*/
+
+    public string StringValidation(string input)
+    {
+        string ValidString = " ";
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.Write("Name cannot be blank: ");
+            input = Console.ReadLine();
+        }
+        //ValidString.
+        return ValidString;
+    }
+    /*---------------//remove an employee--------------------*/
+
+    public void RemoveEmployee(int input)
+    {
+        //Employee _employees;
+        Console.Write("Enter Employee Id to remove: ");
+
+        input = int.Parse(Console.ReadLine());
+        Employee foundEmployeet = _employees.FirstOrDefault(d => d.Id == input);
+        _employees.Remove(foundEmployeet);
+
+        Console.WriteLine($"Employee #{input} was removed from database.");
+
+        Console.Write("Press enter to exit.");
+        Console.ReadLine();
+    }
+
+
+
+
+
+
+
+
 
     public void PrintEmployeesToConsol()
     {
--- Yosemite-QuikTrip/StoreRepository.cs
+++ Yosemite-QuikTrip/StoreRepository.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Yosemite_QuikTrip
+﻿namespace Yosemite_QuikTrip;
+public class StoreRepository
 {
-    internal class StoreRepository
+    // we're using a static list here instead of a proper database
+    // but the general idea still holds up once we cover databases
+    // it's just another piece of code to replace and refactor nbd
+    static List<Stores> _stores = new List<Stores>();
+
+    public List<Stores> GetStores()
     {
+        return _stores;
+    }
+
+    public void SaveNewStore()
+    {
+        Console.Clear();
+
+        Stores newStore = new Stores("", "", 0);
+
+        Console.Write("Store Name: ");
+        newStore.Name = Console.ReadLine();
+
+        Console.Write("Store Manager: ");
+        newStore.Manager = Console.ReadLine();
+
+        Console.Write("Store Sales: ");
+        newStore.Sales = int.Parse(Console.ReadLine());
+
+        _stores.Add(newStore);
+
+        Console.WriteLine(@$"
+==================================
+District {newStore.Name} added to database!
+Manager: {newStore.Manager}
+Sales: {newStore.Sales}
+==================================
+");
+
+        Console.Write("Press enter to exit.");
+        Console.ReadLine();
+    }
+
+    public void UpdateStore(Stores stores)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveStore()
+    {
+        Console.Write("Enter Store name to remove: ");
+
+        string name = Console.ReadLine().ToLower();
+        Stores foundStore = _stores.FirstOrDefault(d => d.Name.ToLower() == name);
+        _stores.Remove(foundStore);
+
+        Console.WriteLine($"Store {name.ToUpper()} was removed from database.");
+
+        Console.Write("Press enter to exit.");
+        Console.ReadLine();
+
+
     }
 }
+
